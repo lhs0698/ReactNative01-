738dda15c65593af66f68533a761f695ffb4d3fb
lhs0698 <yohs0698@naver.com>
2022-08-23 22:02:15 +0900

    weather icon add

--- App.js
+++ App.js
@@ -1,69 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View, Image, TextInput} from 'react-native';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
-
+import Login from './component/Login';
+import Weather from './component/WeatherTest';
 
 export default function App() {
   
   return (
-    <SafeAreaView style={styles.container}>
-      <TextInput style={styles.idInput} autoCapitalize = 'none'  placeholder='ID'/>
-      <TextInput style={styles.pwInput} autoCapitalize = 'none' secureTextEntry={true} placeholder='PASSWORD'/>
-      // autoCapitalize는 첫글자는 대문자 기능 막기 
-      <TouchableOpacity style={styles.loginButton}><Text style={styles.loginText}>로그인</Text></TouchableOpacity>
-      <TouchableOpacity style={styles.newUser}><Text style={styles.loginText}>회원가입</Text></TouchableOpacity>
-    </SafeAreaView>
-  );
+    <>
+      {/* <Login></Login> */}
+      <Weather></Weather>
+    </>
+    
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  idInput:{
-    width: 180,
-    height: 50,
-    borderColor : "gray",
-    borderWidth: 1,
-    borderRadius: 30,
-    marginTop: 300,
-    textAlign: "center"
-  },
-  pwInput:{
-    width: 180,
-    height: 50,
-    borderColor : "gray",
-    borderWidth: 1,
-    borderRadius: 30,
-    marginTop: 10,
-    textAlign: "center"
-  },
-  mainImg:{
-    width: 300,
-    height: 300,
-    margin: "auto"
-  },
-  loginButton: {
-    width: 180,
-    height: 50,
-    backgroundColor: '#01d0fa',
-    borderRadius: 30,
-    justifyContent:"center",
-    marginTop: 30,
-  },
-  newUser:{
-    width: 180,
-    height: 50,
-    backgroundColor: '#01d0fa',
-    borderRadius: 30,
-    justifyContent:"center",
-    marginTop: 10
-  },
-  loginText: {
-    color: 'black',
-    textAlign: "center",
-  }
-});
